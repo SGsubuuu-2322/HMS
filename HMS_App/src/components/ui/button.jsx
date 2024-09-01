9d1653d916572b78ce484a864ec249a8e171ae97
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-09-01 23:18:27 +0530

    Added some necessary packages to our app folder...

--- HMS_App/src/components/ui/button.jsx
+++ HMS_App/src/components/ui/button.jsx
@@ -18,7 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        primary: "bg-[#DC143C] text-white active:scale-110 active:duration-300",
+        primary: "bg-[#DC143C] text-white active:scale-110 active:duration-500",
       },
       size: {
         default: "h-10 px-4 py-2",
