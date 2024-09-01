71bc184bdc5e31c18477b6739a15c4b980c7b104
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-09-01 22:10:50 +0530

    Created and setup the folder for HMS frontend app...

---
+++ HMS_App/src/App.jsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <p className="bg-red-600">
+        Click on the Vite and React logos to learn more
+      </p>
+      <Button variant="primary" className="mx-96">
+        Button
+      </Button>
+    </>
+  );
+}
+
+export default App;
