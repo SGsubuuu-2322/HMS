69438e6a1468ee175205305fc240f3e1e39b8826
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-09-25 16:03:12 +0530

    Created most of the part of Contact page

--- HMS_App/src/Pages/Contact.jsx
+++ HMS_App/src/Pages/Contact.jsx
@@ -1,7 +1,15 @@
 // import React from 'react'
 
+import Footer from "@/components/ui/Footer";
+import ReachUs from "@/components/ui/ReachUs";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <ReachUs />
+      <Footer />
+    </>
+  );
 };
 
 export default Contact;
