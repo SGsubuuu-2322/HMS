e0acd8c13d6c77adbfb36bfd529f28652f21dcf2
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-10-28 23:54:05 +0530

    Added some cool animation...

--- HMS_App/src/layout/AuthLayout.jsx
+++ HMS_App/src/layout/AuthLayout.jsx
@@ -1,16 +1,44 @@
 // import React from 'react'
 import { AppSidebar } from "@/components/custom/AppSidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import logo from "../assets/Hi2.png";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 const AuthLayout = ({ children }) => {
+  const location = useLocation();
+  const [route, setRoute] = useState("Dashboard");
+  useEffect(() => {
+    // console.log(location.pathname.split("/")[2]);
+    setRoute(location.pathname.split("/")[2]);
+  }, [location]);
   return (
     <div className="w-full h-screen overflow-hidden">
       {/* <AuthNav /> */}
       <SidebarProvider>
         <AppSidebar />
         <main className="relative w-full h-screen overflow-y-auto">
-          <div className="z-100 fixed flex ">
+          <div className="z-100 fixed flex  w-full h-20">
             <SidebarTrigger className="hover:bg-transparent" />
-            <h1>Dashboard</h1>
+            <div className="bg-[#0077ff3a] w-full h-full flex items-center">
+              <Link
+                to={"/user/dashboard"}
+                className="image-container hover:scale-110 duration-300 w-44 h-28 rounded-md overflow-hidden"
+              >
+                <img
+                  src={logo}
+                  alt="logo"
+                  className="w-full h-full object-contain"
+                />
+              </Link>
+              <div className="h-full w-1 bg-[#0077ff]"></div>
+              <h1
+                key={route}
+                className="animate-opacityFade font-alice font-bold ml-1 text-[#0077ff94]"
+              >
+                {route.toUpperCase()}
+              </h1>
+            </div>
           </div>
           {children}
         </main>
