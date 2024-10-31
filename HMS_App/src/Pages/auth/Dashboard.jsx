4765c39ec74baf1058dca081b6e0e7c431047fc2
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-10-31 23:20:25 +0530

    Added some hyperlinks to some dashboard items...

--- HMS_App/src/Pages/auth/Dashboard.jsx
+++ HMS_App/src/Pages/auth/Dashboard.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 import { Skeleton } from "@/components/ui/skeleton";
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 
 const Dashboard = () => {
@@ -27,29 +27,39 @@ const Dashboard = () => {
         <div className="relative z-10 grid auto-rows-min gap-4 md:grid-cols-3 w-full">
           {/* <Skeleton className="aspect-video rounded-xl" />{" "} */}
           <div className="bg-[#ffffff88] aspect-video border-2 border-[#0077ff94] rounded-lg flex flex-col items-center justify-around">
-            <h1 className="font-bold text-[#0077ff94]">Outbreaks</h1>
-            <div className="text-4xl text-red-600 font-bold">1</div>
-            <p>View ➡️</p>
-          </div>
-          <div className="bg-[#ffffff88] aspect-video border-2 border-[#0077ff94] rounded-lg flex flex-col items-center justify-around">
             <h1 className="font-bold text-[#0077ff94]">Doctors</h1>
             <div className="text-4xl text-red-600 font-bold">1</div>
-            <p>View ➡️</p>
+            <NavLink to={"/user/doctors/record"} className="cursor-pointer">
+              View ➡️
+            </NavLink>
           </div>
           <div className="bg-[#ffffff88] aspect-video border-2 border-[#0077ff94] rounded-lg flex flex-col items-center justify-around">
             <h1 className="font-bold text-[#0077ff94]">Patients</h1>
             <div className="text-4xl text-red-600 font-bold">1</div>
-            <p>View ➡️</p>
+            <NavLink to={"/user/patients/record"} className="cursor-pointer">
+              View ➡️
+            </NavLink>
           </div>
           <div className="bg-[#ffffff88] aspect-video border-2 border-[#0077ff94] rounded-lg flex flex-col items-center justify-around">
             <h1 className="font-bold text-[#0077ff94]">Appointments</h1>
             <div className="text-4xl text-red-600 font-bold">1</div>
-            <p>View ➡️</p>
+            <NavLink to={"/user/appointments"} className="cursor-pointer">
+              View ➡️
+            </NavLink>
+          </div>
+          <div className="bg-[#ffffff88] aspect-video border-2 border-[#0077ff94] rounded-lg flex flex-col items-center justify-around">
+            <h1 className="font-bold text-[#0077ff94]">Outbreaks</h1>
+            <div className="text-4xl text-red-600 font-bold">1</div>
+            <NavLink to={"/user/likely/outbreaks"} className="cursor-pointer">
+              View ➡️
+            </NavLink>
           </div>
           <div className="bg-[#ffffff88] aspect-video border-2 border-[#0077ff94] rounded-lg flex flex-col items-center justify-around">
             <h1 className="font-bold text-[#0077ff94]">Profile Updation</h1>
             <div className="text-4xl text-red-600 font-bold">1</div>
-            <p>View ➡️</p>
+            <NavLink to={"/user/profile/update"} className="cursor-pointer">
+              View ➡️
+            </NavLink>
           </div>
           <Skeleton className="aspect-video rounded-xl" />{" "}
         </div>
